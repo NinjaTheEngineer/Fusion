f9474a770c41815e399cd176395ba2016cbf1cc1
Alexandre Rocha Coutinho <alex.rochacoutinho@gmail.com>
2024-01-17 23:16:02 +0100

    Fusion 2 - 105

--- Assets/Scripts/Photon/Player.cs
+++ Assets/Scripts/Photon/Player.cs
@@ -2,7 +2,7 @@ using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using static NinjaTools.Utils;
 public class Player : NetworkBehaviour {
 
     [SerializeField] PhysxBall _prefabPhysxBall;
@@ -11,7 +11,14 @@ public class Player : NetworkBehaviour {
     [SerializeField] float ballInitImpulse = 10f;
     [SerializeField] float ballSpawnDelay = 0.25f;
 
+    public Material _material;
 
+    [Networked]
+    public bool spawned { get; set; }
+    private ChangeDetector _changeDetector;
+    public override void Spawned() {
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
+    }
     [Networked] TickTimer delay { get; set; }
 
     NetworkCharacterController _cc;
@@ -19,6 +26,7 @@ public class Player : NetworkBehaviour {
 
     public float movementSpeed = 5f;
     void Awake() {
+        _material = GetComponentInChildren<MeshRenderer>().material;
         _cc = GetComponent<NetworkCharacterController>();
         _forward = transform.forward;
     }
@@ -37,26 +45,49 @@ public class Player : NetworkBehaviour {
             }
 
             if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON0)) {
-                delay = TickTimer.CreateFromSeconds(Runner, ballSpawnDelay);
-                Runner.Spawn(_prefabBall,
-                             transform.position + _forward,
-                             Quaternion.LookRotation(_forward),
-                             Object.InputAuthority,
-                             (runner, o) => {
-                                 o.GetComponent<Ball>().Init();
-                             });
+                SpawnBall();
             }
-
-            if(data.buttons.IsSet(NetworkInputData.MOUSEBUTTON1)) {
-                delay = TickTimer.CreateFromSeconds(Runner, ballSpawnDelay);
-                Runner.Spawn(_prefabPhysxBall,
-                             transform.position + _forward,
-                             Quaternion.LookRotation(_forward),
-                             Object.InputAuthority,
-                             (runner, o) => {
-                                 o.GetComponent<PhysxBall>().Init(ballInitImpulse * _forward);
-                             });
+            if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON1)) {
+                SpawnPhysxBall();
             }
         }
+    }
+    public void SpawnPhysxBall() {
+        var logId = "SpawnPhysxBall";
+        logd(logId, "Spawning PhysxBall");
+        delay = TickTimer.CreateFromSeconds(Runner, ballSpawnDelay);
+        Runner.Spawn(_prefabPhysxBall,
+                    transform.position + _forward,
+                    Quaternion.LookRotation(_forward),
+                    Object.InputAuthority,
+                    (runner, o) => {
+                        o.GetComponent<PhysxBall>().Init(ballInitImpulse * _forward);
+                    });
+        spawned = !spawned;
+    }
+    public void SpawnBall() {
+        var logId = "SpawnBall";
+        logd(logId, "Spawning Ball");
+        delay = TickTimer.CreateFromSeconds(Runner, ballSpawnDelay);
+        Runner.Spawn(_prefabBall,
+                    transform.position + _forward,
+                    Quaternion.LookRotation(_forward),
+                    Object.InputAuthority,
+                    (runner, o) => {
+                        o.GetComponent<Ball>().Init();
+                    });
+        spawned = !spawned;
+    }
+    public override void Render() {
+        var logId = "Render";
+        foreach (var change in _changeDetector.DetectChanges(this)) {
+            logd(logId, $"Change: {change}");
+            switch (change) {
+                case nameof(spawned):
+                    _material.color = Color.white;
+                    break;
+            }
+        }
+        _material.color = Color.Lerp(_material.color, Color.blue, Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,9 @@
+using NinjaTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColor : NinjaMonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+}
